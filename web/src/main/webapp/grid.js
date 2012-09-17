c3956f998d0870122baffe405f00e571992c5c37
Joe <joe.burbano@servicemesh.com>
2012-09-17 11:30:55 -0500

    091712-4

--- web/src/main/webapp/grid.js
+++ web/src/main/webapp/grid.js
@@ -48,7 +48,7 @@ var iNettuts = {
                 $('<a href="#" class="remove">CLOSE</a>').mousedown(function (e) {
                     e.stopPropagation();    
                 }).click(function () {
-                    if(confirm('091712-3 This dam widget will be removed, ok?')) {
+                    if(confirm('091712-4 This widget will be removed, ok?')) {
                         $(this).parents(settings.widgetSelector).animate({
                             opacity: 0    
                         },function () {
