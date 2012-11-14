fb0b12d3aae13e515c6aa08e44d055c8507650a0
Joe <joe.burbano@servicemesh.com>
2012-11-14 09:49:07 -0600

    test1

--- web/src/main/webapp/grid.js
+++ web/src/main/webapp/grid.js
@@ -48,7 +48,7 @@ var iNettuts = {
                 $('<a href="#" class="remove">CLOSE</a>').mousedown(function (e) {
                     e.stopPropagation();    
                 }).click(function () {
-                    if(confirm('102412-1 This widget will be removed, ok?')) {
+                    if(confirm('111412-1t This widget will be removed, ok?')) {
                         $(this).parents(settings.widgetSelector).animate({
                             opacity: 0    
                         },function () {
